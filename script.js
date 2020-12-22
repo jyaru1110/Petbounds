b7578ee8908e0f9247c49cb02308881ec069a4fc
Jyaru <jyaru1110@gmail.com>
2020-12-22 13:24:13 -0600

    Primer commit

---
+++ script.js
@@ -0,0 +1,26 @@
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+                
+function currentDiv(n) {
+    showDivs(slideIndex = n);
+}
+                
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("sac1");
+    var dots = document.getElementsByClassName("w3-badge");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length}
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace( "black", "");
+    }
+    x[slideIndex-1].style.display = "flex";  
+    dots[slideIndex-1].className += " black";
+}
