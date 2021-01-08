be9b6cc86dac24f41671cce55877dc4e823f4113
Carlos Lecona <nataslecval@gmail.com>
2021-01-08 02:41:07 -0600

    Perfil sin foto

---
+++ javascript/perfilMod.js
@@ -0,0 +1,40 @@
+function selecUsu() {
+    var correo = Cookies.get('correo');
+    var enlace = 'http://localhost:3000/api/mostrarUsu?correo=' + correo;
+        $.ajax({
+            type: 'GET',
+            url: enlace,
+            success: response => {
+                var perfil = response.resultado;
+                $('#nom').val(perfil.Nombre_Usuario);
+                $('#nick').val(perfil.Nickname_Usuario);
+                $('#correo').text(perfil.Correo_Usuario);
+                var cadena = perfil.Nacimiento_Usuario;
+                cadena = cadena.slice(0, 10);
+                $('#fecha').val(cadena);
+                var genero = perfil.Genero_Usuario;
+                $('#gen').text(genero);
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
+}
+function modUsu(){
+    var id = Cookies.get('id');
+    var nombre = $('#nom').val();
+    var nick = $('#nick').val();
+    var nacimiento = $('#fecha').val();
+    var enlace = 'http://localhost:3000/api/modUsu?id=' + id + '&nom=' + nombre + '&pat=null&mat=null&nick=' + nick + '&nac=' + nacimiento;
+    $.ajax({
+        type: 'PUT',
+        url: enlace,
+        success: response => {
+            location.reload();
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    });
+    return false;
+}
